53820d51f812bdface3284bb9212b85ad0ab4700
walwizard <goneisgone@hotmail.com>
2023-01-07 09:20:31 -0300

    Exercicio 1

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,11 +1,14 @@
 import react from 'react';
 
 import Helloworld from './Hello';
+import Lorem from './lorem';
 
 function App() {
   return (
     <div>
       <Helloworld />
+      <hr/>
+      <Lorem />
     </div>
   );
 }
---
+++ my-first-app/src/lorem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Lorem = () => {
+    return (
+        <h1>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti consequatur magnam non, dolore asperiores ducimus tempore hic incidunt. Tempora quia iste ipsum tempore similique. Molestias sit blanditiis tenetur aliquid error?
+        </h1>
+    )
+}
+
+export default Lorem;
