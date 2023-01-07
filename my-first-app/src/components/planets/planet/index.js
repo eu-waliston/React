3a0a9ef57a62dfc954acdceeaa6d58b15f30623a
walwizard <goneisgone@hotmail.com>
2023-01-07 09:59:02 -0300

    Dividindo e reutilizando components

--- my-first-app/src/components/planets/planet/index.js
+++ my-first-app/src/components/planets/planet/index.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from "react";
-import './index.css'
+import GrayImg from "../../shared/gray_img";
 
 const Planet = () => {
   return (
@@ -8,7 +8,7 @@ const Planet = () => {
         <p>
         Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes.
         </p>
-        <img className="mercurio" src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv" alt="Mercurio"/>
+        <GrayImg />
     </div>
   );
 };
---
+++ my-first-app/src/components/shared/gray_img/index.js
@@ -0,0 +1,14 @@
+import Ract from "react";
+import "./index.css";
+
+const GrayImg = () => {
+  return (
+    <img
+      className="mercurio"
+      src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv"
+      alt="Mercurio"
+    />
+  );
+};
+
+export default GrayImg;
