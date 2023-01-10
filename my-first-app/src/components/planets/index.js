57f9e57e874c1bf9b97a600423e604220d7e5cc5
walwizard <goneisgone@hotmail.com>
2023-01-10 13:40:37 -0300

    l

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -14,9 +14,6 @@ const Planets = () => {
       <Planet
         name="Mercurio"
 
-        desc="   Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes."
-
-
         link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
 
         desc_link_name="saiba mais"
