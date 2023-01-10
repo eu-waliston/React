77e4d060715eacb39308b2a96d387d8bce4a6216
walwizard <goneisgone@hotmail.com>
2023-01-10 14:34:35 -0300

    Criando uma classe com estado

--- my-first-app/src/components/planets/Planets.js
+++ my-first-app/src/components/planets/Planets.js
@@ -3,42 +3,58 @@ import Planet from "./planet/Planet";
 
 const clickOnPlanet = (name) => {
   alert(`Ùm click no planeta ${name}`);
-}
-
-const Planets = () => {
-  return (
-    <Fragment>
-      <h3>Planet list</h3>
-      <button>Click Here</button>
-      <hr />
-      <Planet
-        name="Mercurio"
-
-        link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-
-        desc_link_name="saiba mais"
-
-        img_url="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv"
-        alt="Mercurio"
-        clickOnPlanet={clickOnPlanet}
-
-        title_with_underline={true}
-
-      />
-
-      <Planet 
-        name="Plutão"
-        desc="Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno. "
-
-
-        img_url="https://upload.wikimedia.org/wikipedia/commons/e/ef/Pluto_in_True_Color_-_High-Res.jpg"
-        clickOnPlanet={clickOnPlanet}
-
-        gray={true}
-      />
-      
-    </Fragment>
-  );
 };
+
+class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planets: [
+        {
+          name: "Mercurio",
+          desc: "Mercurio, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno. ",
+          link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
+          desc_link_name: "saiba mais",
+          img_url:
+            "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv",
+          alt: "Mercurio",
+        },
+        {
+          name: "Plutão",
+          desc: "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno. ",
+          link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
+          desc_link_name: "saiba mais",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/e/ef/Pluto_in_True_Color_-_High-Res.jpg",
+          alt: "Mercurio",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Planet list</h3>
+        <button>Click Here</button>
+        <hr />
+
+        {this.state.planets.map((planet) => 
+        
+          <Planet
+            name={planet.name}
+            desc={planet.desc}
+            link={planet.link}
+            desc_link_name={planet.desc_link_name}
+            img_url={planet.img_url}
+            alt={planet.alt}
+          />
+        )}
+
+      </Fragment>
+    );
+  }
+}
 
 export default Planets;
--- my-first-app/src/components/planets/planet/Planet.js
+++ my-first-app/src/components/planets/planet/Planet.js
@@ -2,32 +2,16 @@ import React, { Fragment } from "react";
 import GrayImg from "../../shared/gray_img/Gray_img";
 
 const Planet = (props) => {
-  const names = ["A", "B", "C", "D"];
-
-  let title;
-  if (props.title_with_underline) {
-    title = (
-      <h4>
-        <u>{props.name}</u>
-      </h4>
-    );
-  } else {
-    title = <h4>{props.name}</h4>;
-  }
 
   return (
     <div onClick={() => props.clickOnPlanet(props.name)}>
-      {title}
+      <h4>{props.name}</h4>
       <p>{props.desc}</p>
       <a href={props.link} target="_blank">
         {props.desc_link_name}
       </a>
       <br />
-      <GrayImg img_url={props.img_url} gray={props.gray} />
-
-      <h4>Satélites:</h4>
-      {names.map((n) => <li>Satélite{n}</li>)}
-      <hr />
+      <GrayImg img_url={props.img_url}/>
     </div>
   );
 };
