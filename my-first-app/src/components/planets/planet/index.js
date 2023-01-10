d336685321a2dcea0bf92f618ec3232d9a4a7434
walwizard <goneisgone@hotmail.com>
2023-01-10 13:46:32 -0300

    Renderizando múltiplos components

--- my-first-app/src/components/planets/planet/index.js
+++ my-first-app/src/components/planets/planet/index.js
@@ -2,20 +2,33 @@ import React, { Fragment } from "react";
 import GrayImg from "../../shared/gray_img";
 
 const Planet = (props) => {
+  const names = ["A", "B", "C", "D"];
+  const satellites = names.map((n) => <li>Satélite {n}</li>);
+
   let title;
-  if(props.title_with_underline) {
-    title = <h4><u>{props.name}</u></h4>  
+  if (props.title_with_underline) {
+    title = (
+      <h4>
+        <u>{props.name}</u>
+      </h4>
+    );
   } else {
-    title =  <h4>{props.name}</h4>
+    title = <h4>{props.name}</h4>;
   }
 
   return (
     <div onClick={() => props.clickOnPlanet(props.name)}>
       {title}
       <p>{props.desc}</p>
-      <a href={props.link} target="_blank">{props.desc_link_name}</a>
+      <a href={props.link} target="_blank">
+        {props.desc_link_name}
+      </a>
       <br />
-      <GrayImg img_url={props.img_url} gray={props.gray}/>
+      <GrayImg img_url={props.img_url} gray={props.gray} />
+
+      <h4>Satélites:</h4>
+      <ul>{satellites}</ul>
+      <hr />
     </div>
   );
 };
