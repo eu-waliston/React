7d730da7dddb4e8a02b75c6deb7f2a1782620855
walwizard <goneisgone@hotmail.com>
2023-01-12 16:51:46 -0300

    Transformando Planet em componente de funcção e adicionando Hooks

--- my-first-app/src/components/planets/Planets.js
+++ my-first-app/src/components/planets/Planets.js
@@ -3,9 +3,7 @@ import Planet from "./planet/Planet";
 
 async function getPlanets() {
   let response = await fetch("http://localhost:3000/api/planets.json");
-
   let data = await response.json();
-  console.log(":)");
   return data;
 }
 
--- my-first-app/src/components/planets/planet/Planet.js
+++ my-first-app/src/components/planets/planet/Planet.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import GrayImg from "../../shared/gray_img/Gray_img";
 
 async function getSatellites(id) {
@@ -8,48 +8,34 @@ async function getSatellites(id) {
   return data;
 }
 
-class Planet extends React.Component {
-  constructor(props) {
-    super(props);
+const Planet = (props) => {
+  const [satellites, setSatelites] = useState([]);
 
-    this.state = {
-      satellites: [],
-    };
-  }
+  useEffect(() => {
+    getSatellites(props.id).then((data) => {
+      setSatelites(data["satellites"]);
+    });
+  }, []);
 
-  componentDidMount() {
-    getSatellites(this.props.id).then(data => {
-      this.setState(state => ({
-        satellites: data["satellites"]
-      }))
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h4>{this.props.name}</h4>
-        <h4>Satélites</h4>
-        <p>{this.props.desc}</p>
-        <a href={this.props.link} target="_blank">
-          {this.props.desc_link_name}
-        </a>
-        <br />
-        <GrayImg img_url={this.props.img_url} />
-        <h4>Satélites</h4>
-        <ul>
-          {
-            this.state.satellites.map((satellite, index) =>
-              <li key={index}>
-                {satellite.name}
-              </li>
-            )
-          }
-        </ul>
-        <hr />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h4>{props.name}</h4>
+      <h4>Satélites</h4>
+      <p>{props.desc}</p>
+      <a href={props.link} target="_blank">
+        {props.desc_link_name}
+      </a>
+      <br />
+      <GrayImg img_url={props.img_url} />
+      <h4>Satélites</h4>
+      <ul>
+        {satellites.map((satellite, index) => (
+          <li key={index}>{satellite.name}</li>
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
+};
 
 export default Planet;
