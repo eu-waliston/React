bae5bc36c657832c2859471b698249af80e6e43d
walwizard <goneisgone@hotmail.com>
2023-01-12 16:08:59 -0300

    Refatorando nosso Lifecycle com hooks

--- my-first-app/src/components/planets/Planets.js
+++ my-first-app/src/components/planets/Planets.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Planet from "./planet/Planet";
 
 async function getPlanets() {
@@ -9,28 +9,14 @@ async function getPlanets() {
   return data;
 }
 
-/*
-componentDidMount() {
-  getPlanets().then((data) => {
-    setState((state) => ({
-      planets: data["planets"],
-    }));
-  });
-}
-*/
-
 const Planets = () => {
-  const [planets, setPlanets] = useState([
-    {
-      id: "mars",
-      name: "Mars",
-      desc: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.",
-      img_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
-      link: "saiba +",
-      desc_link_name: "https://en.wikipedia.org/wiki/Mars",
-    },
-  ]);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    getPlanets().then((data) => {
+      setPlanets(data["planets"])
+    });
+  }, [])
 
   const removeLast = () => {
     let new_planets = [...planets];
