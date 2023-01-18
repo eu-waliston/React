df054dd7b8ffa9cb65a2aba953b88efbd4eaa915
walwizard <goneisgone@hotmail.com>
2023-01-17 21:46:21 -0300

    Atualizações dos arquivos de rotas

--- my-first-app/src/routes.js
+++ my-first-app/src/routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import PlanetsScrren from "./screens/planets";
+import PlanetScreen from './screens/planet';
 
 const Rotas = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<PlanetsScrren />} />
+            <Route exact path="/planet" element={<PlanetScreen />} />
         </Routes>
     </BrowserRouter>
 )
