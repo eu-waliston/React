00565442664beea6014f174f2ca687f8d0a24027
walwizard <goneisgone@hotmail.com>
2023-02-09 16:06:29 -0300

    This chapter cover the HTML, and JS acpect in react and a couple bootstrap

--- Pro_React_16/chapter_3/primer/src/App.js
+++ Pro_React_16/chapter_3/primer/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 4
+    }
+  }
+
+  isEven(val) {
+    return val % 2 === 0 ? "Even" : "Odd"
+  }
+
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => {
+    this.setState({ cout: this.state.count + 1 });
+  }
+
+
+  render = () =>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h4 className={this.getClassName(this.state.count)}>
+        <button
+          className='btn btn-info m-2'
+          onClick={this.handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          Click Me!
+        </button>
+        Number of things: {this.isEven(this.state.count)}
+      </h4>
+
     </div>
-  );
 }
 
-export default App;
