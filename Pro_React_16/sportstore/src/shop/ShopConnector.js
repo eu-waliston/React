c1f8eb3b4460709805d71d2f5c616accd1c359a0
walwizard <goneisgone@hotmail.com>
2023-02-14 19:41:20 -0300

    Connecting to the Data Store and the URL Router

---
+++ Pro_React_16/sportstore/src/shop/ShopConnector.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { connect } from "react-redux";
+import { loadData } from "../data/ActionCreators";
+import { DataTypes } from "../data/Types";
+
+import { Shop } from "./Shop";
+
+const mapStateProps = (dataStore) => ({
+    ...dataStore
+})
+
+const mapDispatchToProps = {
+    loadData
+}
+
+const filterProducts = (products = [], category) =>
+    (!category || category === "All")
+        ? products
+        : products.filter(p => p.category.toLowerCase() === category.toLowerCase());
+
+export const ShopConnector = connect(mapStateProps, mapDispatchToProps)(
+    class extends Component {
+        render() {
+            return <Switch>
+                <Route
+                    path={"/shop/products/:category"}
+                    render={(routeProps) =>
+                        <Shop
+                            {...this.props}
+                            {...routeProps}
+                            products={filterProducts(this.props.products, routeProps.match.params.category)}
+                        />}
+                />
+                <Redirect to="/shop/products" />
+            </Switch>
+        }
+
+        componentDidMount() {
+            this.props.loadData(DataTypes.CATEGORIES);
+
+            this.props.loadData(DataTypes.PRODUCTS);
+
+
+        }
+    }
+)
