445821e0f20f1227d631bd825cc942a1faf576bd
walwizard <goneisgone@hotmail.com>
2023-02-14 15:18:03 -0300

    Product and Category Components

---
+++ Pro_React_16/sportstore/src/shop/CategoryNavigation.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class CategoryNavigation extends Component {
+    render() {
+        return <React.Fragment>
+            <Link
+                className="btn btn-secondary btn-block"
+                to={this.props.baseUrl}
+            >All</Link>
+
+            {this.props.categories && this.props.categories.map(cat =>
+                <Link className="btn btn-secondary btn-block" key={cat}
+                    to={`${this.props.baseUrl}/${cat.toLowerCase()}`}
+                >
+                    {cat}
+                </Link>
+            )}
+        </React.Fragment>
+    }
+}
---
+++ Pro_React_16/sportstore/src/shop/ProductList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export class ProductList extends Component {
+    render() {
+        if (this.props.products == null || this.props.products.length == 0) {
+            return <h5 className="p-2">No PRoducts</h5>
+        }
+
+        return this.props.products.map(p =>
+            <div className="card m-1 p-1 bg-light" key={p.id}>
+                <h4>
+                    {p.name}
+                    <span
+                        className="badge badge-pill badge-primary float-right"
+                    >
+                        {p.price.toFixed(2)}
+                    </span>
+                </h4>
+
+                <div className="card-text bg-white p-1">
+                    {p.description}
+                </div>
+            </div>)
+    }
+
+}
