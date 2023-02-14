daeb3503a05e470f6e5891be113750efcd786d7c
walwizard <goneisgone@hotmail.com>
2023-02-14 14:28:30 -0300

    Sport Store Webservices and Packages

--- Pro_React_16/sportstore/src/index.js
+++ Pro_React_16/sportstore/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Font Aweasome e Bootstrap  */
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap/dist/css/bootstrap.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
