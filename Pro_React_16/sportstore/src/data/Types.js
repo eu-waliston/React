e0cf3c723211fbfbf969d61bd055cd323eba4e32
walwizard <goneisgone@hotmail.com>
2023-02-16 13:53:28 -0300

    ActionTypes to Add, Remove, Delete and Clear Shop Cart

--- Pro_React_16/sportstore/src/data/Types.js
+++ Pro_React_16/sportstore/src/data/Types.js
@@ -4,5 +4,9 @@ export const DataTypes = {
 }
 
 export const ActionTypes = {
-    DATA_LOAD: "data_load"
+    DATA_LOAD: "data_load",
+    CART_ADD: "cart-add",
+    CART_UPDATE: "cart_update",
+    CART_REMOVE: "cart_delete",
+    CART_CLEAR: "cart_clear"
 }
